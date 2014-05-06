55406f6a5efea7369893d273246fbb92b3fb97ac
siredmar <knallerbse@gmail.com>
2014-05-06 11:12:57 +0200

    TEIL_B: reviewed schematic and layout

---
+++ gnublin/u-boot_work-2009.11/include/config.h
@@ -0,0 +1,5 @@
+#define CONFIG_EA3131 1
+
+/* Automatically generated - do not edit */
+#include <configs/ea31xx.h>
+#include <asm/config.h>
---
+++ gnublin/u-boot_work-2009.11/include/config.mk
@@ -0,0 +1,3 @@
+ARCH   = arm
+CPU    = lpc313x
+BOARD  = ea31xx
