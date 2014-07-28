2a4046733915d3994565369d57ff20a865bf64cd
siredmar <knallerbse@gmail.com>
2014-07-28 18:30:24 +0200

    updated Documenation

---
+++ Docu/Masterarbeit/Bilder/TeilA/povray/gnublin_ext_ssd1963.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+"/usr/bin/povray" +W1600 +H1200 +A0.3 +FN +L" /usr/share/fonts/" +L"/opt/eagle3d/ulp//../povray" +O"/home/armin/Masterarbeit/Docu/Masterarbeit/Bilder/TeilA/povray/gnublin_ext_ssd1963.png" /home/armin/Masterarbeit/Docu/Masterarbeit/Bilder/TeilA/povray/gnublin_ext_ssd1963.pov 
---
+++ Docu/Masterarbeit/Bilder/TeilB/HDMI_RGB_LVDS_V10_top.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+"/usr/bin/povray" +W1600 +H1200 +A0.3 +FN +L" /usr/share/fonts/" +L"/opt/eagle3d/ulp//../povray" +O"/home/armin/Masterarbeit/Docu/Masterarbeit/Bilder/TeilB/HDMI_RGB_LVDS_V10_top.png" /home/armin/Masterarbeit/Docu/Masterarbeit/Bilder/TeilB/HDMI_RGB_LVDS_V10_top.pov 
